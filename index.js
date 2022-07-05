cabe6cd9e854a1bb61e2eada80270fcb3e608d0f
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2022-07-05 00:24:12 -0500

    Format random function

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ exports.silly = data.silly;
 exports.serious = data.serious;
 
 exports.random = function random(type) {
-    const items = data[type]
+  const items = data[type];
 
-    if (! items) {
-        return undefined
-    }
+  if (!items) {
+    return undefined;
+  }
 
-    return items[Math.floor(Math.random() * items.length)];
-}
+  return items[Math.floor(Math.random() * items.length)];
+};
